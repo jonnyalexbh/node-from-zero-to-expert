55dc7167e71c4f1612c689d333e5ee434bb989bb
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-12 14:16:24 -0500

    let and var

---
+++ 02-necessary-topics/let-var.js
@@ -0,0 +1,15 @@
+// let name = 'Wolverine';
+
+// if (true) {
+//   name = 'Magento'
+// }
+
+// console.log(name);
+
+let i = 99;
+
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
+
+console.log(`${i}`);
