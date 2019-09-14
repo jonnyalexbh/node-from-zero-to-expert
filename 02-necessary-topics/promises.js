b6ca33c52855c3c438f44cd8eecf006009a2f9e5
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-14 16:55:59 -0500

    promises

---
+++ 02-necessary-topics/promises.js
@@ -0,0 +1,58 @@
+let employees = [{
+  id: 1,
+  name: 'Tankis'
+}, {
+  id: 2,
+  name: 'Jose'
+}, {
+  id: 3,
+  name: 'Juan'
+}];
+
+let salaries = [{
+  user_id: 1,
+  salary: 1000
+}, {
+  user_id: 2,
+  salary: 2000
+}];
+
+let getEmployee = (id) => {
+  return new Promise((resolve, reject) => {
+
+    let employeeDB = employees.find(employee => employee.id === id)
+
+    if (!employeeDB) {
+      reject(`there is no employee with the id ${id}`);
+    } else {
+      resolve(employeeDB);
+    }
+
+  });
+}
+
+let getSalary = employee => {
+  return new Promise((resolve, reject) => {
+    let salaryDB = salaries.find(salary => salary.user_id === employee.id)
+
+    if (!salaryDB) {
+      reject(`no salary was found for the employee ${employee.id}`);
+    } else {
+      resolve({
+        id: employee.id,
+        name: employee.name,
+        salary: salaryDB.salary
+      });
+    }
+  });
+}
+
+getEmployee(3).then(employee => {
+  getSalary(employee).then(resp => {
+    console.log(`the salary of the ${employee.name} is of ${resp.salary}`);
+  }, error => {
+    console.log(error);
+  })
+}, error => {
+  console.log(error);
+});
