1e67f2e13227ea0f6c26dd330da400c299623186
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-15 18:37:50 -0500

    yargs

--- 03-node-bases/app.js
+++ 03-node-bases/app.js
@@ -1,9 +1,23 @@
+const argv = require('yargs')
+  .command('list', 'console print the multiplication table', {
+    base: {
+      demand: true,
+      alias: 'b'
+    },
+    limit: {
+      alias: 'l',
+      default: 10
+    }
+  })
+  .argv
+
 const { createFile } = require('./multiply');
 
-let argv = process.argv;
-let param = argv[2];
-let base = param.split('=')[1];
+// console.log(argv.base);
 
-createFile(base)
-  .then(file => console.log(`file created ${file}`))
-  .catch(error => console.log(error));
+// let param = argv[2];
+// let base = param.split('=')[1];
+
+// createFile(base)
+//   .then(file => console.log(`file created ${file}`))
+//   .catch(error => console.log(error));
