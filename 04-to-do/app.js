45549b58555666cd4fc4841a368f0de6914be7c8
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-19 21:57:49 -0500

    automatic validations with Yargs

--- 04-to-do/app.js
+++ 04-to-do/app.js
@@ -1,4 +1,4 @@
-const argv = require('yargs').argv
+const argv = require('./config/yargs').argv;
 
 console.log(argv);
 
---
+++ 04-to-do/config/yargs.js
@@ -0,0 +1,25 @@
+const argv = require('yargs')
+  .command('create', 'create an item to do', {
+    description: {
+      demand: true,
+      alias: 'd',
+      desc: 'description of the task to be done'
+    }
+  })
+  .command('update', 'update the completed status of a task', {
+    description: {
+      demand: true,
+      alias: 'd',
+      desc: 'description of the task to be done'
+    },
+    completed: {
+      default: true,
+      alias: 'c',
+      desc: 'mark the task completed or pending'
+    }
+  })
+  .argv
+
+module.exports = {
+  argv
+}
