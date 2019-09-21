e5869340450a4c6cf76bc21e46b63bb70b294205
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-21 14:33:52 -0500

    serve static content

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  let output = {
-    name: 'jonnyalex.bh',
-    phone: 2224567,
-    enabled: true
-  }
-  res.send(output)
-})
+app.use(express.static(__dirname + '/public'));
 
-app.get('/data', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   let output = {
+//     name: 'jonnyalex.bh',
+//     phone: 2224567,
+//     enabled: true
+//   }
+//   res.send(output)
+// })
+
+// app.get('/data', function (req, res) {
+//   res.send('Hello World')
+// })
 
 app.listen(3000, () => {
   console.log('listening to requests in the port 3000');
