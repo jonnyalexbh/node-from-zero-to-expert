c2a08710613f9be510ee4550fd6f6baa7f949885
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-20 19:00:10 -0500

    HTTP requests - axios - request

--- 05-world-weather/app.js
+++ 05-world-weather/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
   address: {
     alias: 'd',
@@ -6,4 +8,16 @@ const argv = require('yargs').options({
   }
 }).argv;
 
-console.log(argv.address);
+const encodeUrl = encodeURI(argv.address);
+
+const instance = axios.create({
+  baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
+  headers: { 'x-rapidapi-key': '9f338e8f57msh3c2d31501a8cd23p1b9fe6jsnd52601e97653' }
+});
+
+instance.get()
+  .then(resp => {
+    console.log(resp.data.Results[0]);
+  }).catch(err => {
+    console.log('ERROR!!!!!', err);
+  });
