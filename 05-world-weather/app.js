b77c7c62f1d927f608eda405cf9d1abaf6e67d45
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-20 20:15:09 -0500

    connecting both services

--- 05-world-weather/app.js
+++ 05-world-weather/app.js
@@ -9,9 +9,23 @@ const argv = require('yargs').options({
   }
 }).argv;
 
-place.getPlaceLatLng(argv.address)
-  .then(console.log)
+// place.getPlaceLatLng(argv.address)
+//   .then(console.log)
 
-weather.getWeather(40.750000, -74.000000)
+// weather.getWeather(40.750000, -74.000000)
+//   .then(console.log)
+//   .catch(console.log)
+
+const getInfo = async (address) => {
+  try {
+    const coords = await place.getPlaceLatLng(address);
+    const temperature = await weather.getWeather(coords.lat, coords.lng);
+    return `the weather of ${coords.address} es de ${temperature}.`;
+  } catch (e) {
+    return `the weather could not be determined ${address}`;
+  }
+}
+
+getInfo(argv.address)
   .then(console.log)
-  .catch(console.log)
+  .catch(console.log);
