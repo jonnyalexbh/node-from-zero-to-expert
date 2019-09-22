db8b72aa89f2c5564a93ed1093f122566e700673
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-21 21:10:13 -0500

    using partials with HBS

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const app = express()
 
+const hbs = require('hbs');
+
 app.use(express.static(__dirname + '/public'));
 
 // express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
   res.render('home', {
     name: 'jonnyalexbh',
+    year: new Date().getFullYear()
+  });
+})
+
+app.get('/about', function (req, res) {
+  res.render('about', {
     year: new Date().getFullYear()
   });
 })
