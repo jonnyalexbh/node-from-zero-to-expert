55f34f6a29a3d748af9e676fa32c2781811e5fbc
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-23 13:10:02 -0500

    socket.io installation

--- 08-basic-socket/server/server.js
+++ 08-basic-socket/server/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 
 const app = express();
+let server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+// IO = this is the backend communication
+let io = socketIO(server);
 
-app.listen(port, (err) => {
+server.listen(port, (err) => {
   if (err) throw new Error(err);
   console.log(`Servidor corriendo en puerto ${port}`);
 });
