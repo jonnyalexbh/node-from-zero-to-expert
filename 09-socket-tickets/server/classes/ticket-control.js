670c413cd531571dc761a60c72a29431118a4db4
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-23 20:34:11 -0500

    collection of pending tickets attend

--- 09-socket-tickets/server/classes/ticket-control.js
+++ 09-socket-tickets/server/classes/ticket-control.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
 
+class Ticket {
+  constructor(number, desk) {
+    this.number = number;
+    this.desk = desk;
+  }
+}
+
 class TicketControl {
   constructor() {
     this.latest = 0;
     this.today = new Date().getDate();
+    this.tickets = [];
 
     let data = require('../data/data.json');
 
     if (data.today === this.today) {
       this.latest = data.latest;
+      this.tickets = data.tickets;
     } else {
       this.resetCount();
     }
@@ -17,6 +26,9 @@ class TicketControl {
 
   nextTicket() {
     this.latest += 1;
+    let ticket = new Ticket(this.latest, null);
+    this.tickets.push(ticket);
+
     this.recordFile();
     return `Ticket ${this.latest}`;
   }
@@ -27,6 +39,7 @@ class TicketControl {
 
   resetCount() {
     this.latest = 0;
+    this.tickets = [];
     console.log('The system has been initialized');
     this.recordFile();
   }
@@ -34,7 +47,8 @@ class TicketControl {
   recordFile() {
     let jsonData = {
       latest: this.latest,
-      today: this.today
+      today: this.today,
+      tickets: this.tickets
     }
 
     let jsonDataString = JSON.stringify(jsonData);
