f518b37d80fb60992739ba6b6b9cfa5f819a8a9b
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-27 09:48:29 -0500

    HTTP requests

--- 07-rest-server/server/server.js
+++ 07-rest-server/server/server.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/user', function (req, res) {
+  res.json('get user')
+})
+
+app.post('/user', function (req, res) {
+  let body = req.body;
+  res.json(body)
+})
+
+app.put('/user/:id', function (req, res) {
+  let id = req.params.id;
+  res.json({
+    id
+  })
+})
+
+app.delete('/user', function (req, res) {
+  res.json('delete user')
 })
 
 app.listen(3000, () => {
