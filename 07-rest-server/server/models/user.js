0798162625e43cd8a86d54a42cc00c9c4e7b95be
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-09-28 13:39:35 -0500

    user model

---
+++ 07-rest-server/server/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'the name is necessary']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'mail is necessary']
+  },
+  password: {
+    type: String,
+    required: [true, 'the password is necessary']
+  },
+  img: {
+    type: String,
+    required: false
+  },
+  role: {
+    type: String,
+    default: 'USER_ROLE',
+    enum: rolesValidos
+  },
+  state: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean,
+    default: false
+  }
+})
+
+module.exports = mongoose.model('User', userSchema);
